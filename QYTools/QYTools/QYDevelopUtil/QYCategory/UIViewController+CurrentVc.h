2fad077b344f12f695425479be4056b685afbd21
zqy <zhangqingyu12@126.com>
2017-05-12 09:53:37 +0800

    no message

---
+++ QYTools/QYTools/QYDevelopUtil/QYCategory/UIViewController+CurrentVc.h
@@ -0,0 +1,15 @@
+//
+//  UIViewController+CurrentVc.h
+//  SunrayWoman
+//
+//  Created by 张玉 on 16/3/31.
+//  Copyright © 2016年 zijing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (CurrentVc)
+
++ (UIViewController*)currentViewController;
+
+@end
---
+++ QYTools/QYTools/QYDevelopUtil/QYDefines.h
@@ -0,0 +1,13 @@
+//
+//  QYDefines.h
+//  QYTools
+//
+//  Created by 张庆玉 on 2017/5/8.
+//  Copyright © 2017年 张庆玉. All rights reserved.
+//
+
+#ifndef QYDefines_h
+#define QYDefines_h
+
+
+#endif /* QYDefines_h */
